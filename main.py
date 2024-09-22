82df788b1dee094e31831fc991d80079c138d7fc
Jannis Lauer <jannislauer@icloud.com>
2024-09-22 18:07:35 +0200

    updatet

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import pandas as pd
 
 Path = "new"
 stockext = "*stock.csv"
-deliveryext = "*delivery.csv"
 
 all_stock_file = [file
                  for path, subdir, files in os.walk(Path)
                  for file in glob(os.path.join(path, stockext))]
 
+data = pd.read_csv("databasestock.csv")
+data['quantity'] = 0
+
 for file in all_stock_file:
-    print("Data")
-    data = pd.read_csv(file)
-    print(data)
+    temp = pd.read_csv(file)
+    data = pd.concat([temp,data], ignore_index=True)
 
 
+data.to_csv("databasestock.csv",mode="w",index=False)
